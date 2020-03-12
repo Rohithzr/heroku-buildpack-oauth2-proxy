e18ca6212f92f48a77094259a5e6140feabfabde
Rohit Hazra <rohithzr@live.com>
2020-03-12 13:58:19 +0530

    trigger build

--- scripts/start_oauth2_proxy.sh
+++ scripts/start_oauth2_proxy.sh
@@ -9,6 +9,8 @@ if [ -z ${OAUTH2_PROXY_PROVIDER+x} ]; then echo "please set OAUTH2_PROXY_PROVIDE
 if [ -z ${OAUTH2_PROXY_CLIENT_ID+x} ]; then echo "please set OAUTH2_PROXY_CLIENT_ID"; exit 1; fi
 if [ -z ${OAUTH2_PROXY_CLIENT_SECRET+x} ]; then echo "please set OAUTH2_PROXY_CLIENT_SECRET"; exit 1; fi
 if [ -z ${OAUTH2_PROXY_COOKIE_SECRET+x} ]; then echo "please set OAUTH2_PROXY_COOKIE_SECRET"; exit 1; fi
+if [ -z ${OIDC_REDIRECT_URL+x} ]; then echo "please set OIDC_REDIRECT_URL"; exit 1; fi
+if [ -z ${OIDC_ISSUER_URL+x} ]; then echo "please set OIDC_ISSUER_URL"; exit 1; fi
 
 OAUTH2_PROXY_SET_XAUTHREQUEST="${OAUTH2_PROXY_SET_XAUTHREQUEST:-true}"
 export OAUTH2_PROXY_SET_XAUTHREQUEST
@@ -28,11 +30,6 @@ else
     OAUTH2_PROXY_EMAIL_DOMAINS="*"
 fi
 export OAUTH2_PROXY_EMAIL_DOMAINS
-
-if [ -n "${OAUTH2_GITHUB_ORG}" ]; then
-    OAUTH2_PROXY_GITHUB_ORG="${OAUTH2_GITHUB_ORG}"
-    export OAUTH2_PROXY_GITHUB_ORG
-fi
 
 echo "starting oauth2_proxy..."
 echo "debug..."
